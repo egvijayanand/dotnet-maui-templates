12dd9d219e29c237c4bed7e5082e733686aaff22
Vijay Anand E G <81947404+egvijayanand@users.noreply.github.com>
2021-08-13 22:16:54 +0530

    .NET CLI Shell item template

--- src/ItemTemplates/MauiTemplatesCLI/Create-Template.bat
+++ src/ItemTemplates/MauiTemplatesCLI/Create-Template.bat
@@ -1,8 +1,8 @@
 :: Creates a new NuGet package from the project file
 @echo off
-if exist .\bin\Debug\VijayAnand.MauiTemplates.1.0.0.nupkg del .\bin\Debug\VijayAnand.MauiTemplates.1.0.0.nupkg
+if exist .\bin\Debug\VijayAnand.MauiTemplates.1.0.1.nupkg del .\bin\Debug\VijayAnand.MauiTemplates.1.0.1.nupkg
 
 echo Creating item template . . .
-dotnet pack .\VijayAnand.MauiTemplates.csproj -p:PackageVersion=1.0.0
+dotnet pack .\VijayAnand.MauiTemplates.csproj -p:PackageVersion=1.0.1
 echo Process completed
 pause
