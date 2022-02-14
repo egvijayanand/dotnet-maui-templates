c4b5c34e206b2dda0e41fe0f36e52ce6a40db520
Vijay Anand E G <81947404+egvijayanand@users.noreply.github.com>
2022-02-14 14:09:44 +0530

    Automation changes

--- src/MauiTemplatesCLI/Uninstall-Template.bat
+++ src/MauiTemplatesCLI/Uninstall-Template.bat
@@ -1,6 +1,27 @@
 :: Removes the installed project template
 @echo off
-echo Uninstalling the item template . . .
-dotnet new --uninstall VijayAnand.MauiTemplates
-echo Process completed
+
+:: Modify .NET SDK Version
+if exist global.json ren global.json global.json.bak
+
+:: Package Name
+
+if not exist PackageName.txt (call Error "Package name file not available." & goto end)
+
+set /P packageName=<PackageName.txt
+
+if [%packageName%]==[] (call Error "Package name not configured." & goto end)
+
+::echo Package Name: %packageName%
+
+call Info ".NET SDK Version"
+dotnet --version
+
+call Info "Uninstalling %packageName% ..."
+
+dotnet new --uninstall %packageName%
+
+if %errorlevel% == 0 (call Success "Process completed.") else (call Error "Package uninstall failed.")
+
+:end
 pause
