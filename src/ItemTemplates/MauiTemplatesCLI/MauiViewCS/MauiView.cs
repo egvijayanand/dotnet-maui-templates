9d2b60fe2b23f5e74bfe768b0bdf685b716ecf70
Vijay Anand E G <81947404+egvijayanand@users.noreply.github.com>
2022-02-14 13:02:02 +0530

    C# Item Templates

---
+++ src/ItemTemplates/MauiTemplatesCLI/MauiViewCS/MauiView.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Maui.Graphics;
+
+namespace MyApp.Namespace
+{
+    public partial class MauiView : ContentView
+    {
+        public MauiView()
+        {
+            Content = new StackLayout()
+            {
+                Children =
+                {
+                    new Label()
+                    {
+                        Text = "Welcome to .NET MAUI!!!",
+                        TextColor = Colors.Purple,
+                        HorizontalOptions = LayoutOptions.Center,
+                        VerticalOptions = LayoutOptions.Center
+                    }
+                }
+            };
+        }
+    }
+}
