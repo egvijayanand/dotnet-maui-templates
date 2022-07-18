9267dfab242d5d4f031252798fdf420dbd278ff2
Vijay Anand E G <81947404+egvijayanand@users.noreply.github.com>
2022-07-19 01:06:53 +0530

    Item templates for Page with ViewModel

---
+++ src/ItemTemplates/MauiPageCSWithViewModel/Views/MauiPageCS.cs
@@ -0,0 +1,25 @@
+﻿using $basenamespace$.ViewModels;
+
+namespace $rootnamespace$
+{
+    public class $safeitemname$ : ContentPage
+    {
+        public $safeitemname$()
+        {
+            BindingContext = new $fileinputname$ViewModel();
+            Content = new StackLayout
+            {
+                Children =
+                {
+                    new Label
+                    {
+                        Text = "Welcome to .NET MAUI!!!",
+                        TextColor = Colors.Purple,
+                        HorizontalOptions = LayoutOptions.Center,
+                        VerticalOptions = LayoutOptions.Center
+                    }
+                }
+            };
+        }
+    }
+}
