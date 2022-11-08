604a5babc5f91dcaf5fe40f23f5ba8eeb68aa4a2
Vijay Anand E G <81947404+egvijayanand@users.noreply.github.com>
2022-11-08 22:20:22 +0530

    CLI template package updated to .NET 7 GA release

--- src/MauiTemplatesCLI/Install-Release-Template.bat
+++ src/MauiTemplatesCLI/Install-Release-Template.bat
@@ -1,33 +1,7 @@
 :: Installs the NuGet package
 @echo off
 
-:: Package Name
-
-if not exist PackageName.txt (call Error "Package name file not available." & goto end)
-
-set /P packageName=<PackageName.txt
-
-if [%packageName%]==[] (call Error "Package name not configured." & goto end)
-
-:: Package Version
-
-if not exist PackageVersion.txt (call Error "Version file not available." & goto end)
-
-set /P packageVersion=<PackageVersion.txt
-
-if [%packageVersion%]==[] (call Error "Version # not configured." & goto end)
-
-:: Modify .NET SDK Version
-::if exist global.json (call Error "Verify the .NET SDK Version" & goto end)
-
-call Info ".NET SDK Version"
-dotnet --version
-
-call Info "Installing the %packageName% Release build template ver. %packageVersion% ..."
-
-dotnet new --install .\bin\Release\%packageName%.%packageVersion%.nupkg
-
-if %errorlevel% == 0 (call Success "Process completed.") else (call Error "Package install failed.")
+call Install-Template.bat Release
 
 :end
 pause
