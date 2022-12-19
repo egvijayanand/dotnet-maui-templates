b7e1dab9ac0996587e2a87ce67763589ba16cd8e
Vijay Anand E G <81947404+egvijayanand@users.noreply.github.com>
2022-12-19 18:28:26 +0530

    Template package updated to support .NET 7 SR1 (v7.0.52)

--- src/MauiTemplatesCLI/Create-Template.bat
+++ src/MauiTemplatesCLI/Create-Template.bat
@@ -21,6 +21,12 @@ set /P packageVersion=<PackageVersion.txt
 
 if [%packageVersion%]==[] (call Error "Version # not configured." & goto end)
 
+:: Check whether the context is git repository or not
+git rev-parse --is-inside-work-tree
+
+:: Retrieve the hash of the latest commit
+if %errorlevel% == 0 (for /F "tokens=*" %%g in ('git rev-parse --short HEAD') do (set revisionId=+sha.%%g)) else (set revisionId=)
+
 :: Modify .NET SDK Version
 ::if exist global.json (call Error "Verify the .NET SDK Version" & goto end)
 ::if exist global.json.net6.bak ren global.json.net6.bak global.json
@@ -30,10 +36,12 @@ dotnet --version
 
 if exist .\bin\%config%\%packageName%.%packageVersion%.nupkg del .\bin\%config%\%packageName%.%packageVersion%.nupkg
 
+echo.
 call Info "Creating %packageName% template ver. %packageVersion% NuGet package in %config% mode ..."
 
-dotnet pack .\VijayAnand.MauiTemplates.csproj -c %config% -p:PackageVersion=%packageVersion%
+dotnet pack .\VijayAnand.MauiTemplates.csproj -c %config% -p:PackageVersion=%packageVersion%%revisionId%
 
+echo.
 if %errorlevel% == 0 (call Success "Process completed.") else (call Error "Package creation failed.")
 
 :: Revert the changes
