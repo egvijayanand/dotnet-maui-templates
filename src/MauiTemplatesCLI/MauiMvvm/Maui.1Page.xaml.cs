439f44b8baa1ee60701c0f2697b10b667b18f02a
Vijay Anand E G <81947404+egvijayanand@users.noreply.github.com>
2023-03-20 19:20:54 +0530

    Added Item templates for Page with ViewModel

---
+++ src/MauiTemplatesCLI/MauiMvvm/Maui.1Page.xaml.cs
@@ -0,0 +1,15 @@
+﻿#if SameFolder
+namespace MyApp.Namespace
+#else
+namespace MyApp.Namespace.Views
+#endif
+{
+    public partial class Maui__1Page : ContentPage
+    {
+        public Maui__1Page(Maui__1ViewModel viewModel)
+        {
+            InitializeComponent();
+            BindingContext = viewModel;
+        }
+    }
+}
---
+++ src/MauiTemplatesCLI/MauiMvvmCS/Maui.1ViewModel.cs
@@ -0,0 +1,14 @@
+﻿#if SameFolder
+namespace MyApp.Namespace
+#else
+namespace MyApp.Namespace.ViewModels
+#endif
+{
+    public partial class Maui__1ViewModel : BaseViewModel
+    {
+        public Maui__1ViewModel()
+        {
+            
+        }
+    }
+}
