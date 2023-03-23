4d3bcfe960b31b39bee591b39df88087b21d6112
Vijay Anand E G <81947404+egvijayanand@users.noreply.github.com>
2023-03-24 04:09:01 +0530

    v3.1.0.0 release of VS Extension

---
+++ src/ItemTemplates/MauiShellCS/MauiShell.cs
@@ -0,0 +1,47 @@
+﻿namespace $rootnamespace$
+{
+    public partial class $safeitemname$ : Shell
+    {
+        public $safeitemname$()
+        {
+            // Flyout Items
+            //Items.Add(new FlyoutItem()
+            //{
+            //    Title = "",
+            //    Items =
+            //    {
+            //        new ShellContent()
+            //        {
+            //            ContentTemplate = new DataTemplate(typeof(ContentPage)),
+            //            Route = "",
+            //        },
+            //    }
+            //});
+
+            // Menu Items
+            //Items.Add(new MenuItem()
+            //{
+            //    Text = "",
+            //}.Invoke(x => x.Clicked += OnMenuItemClicked));
+
+            //TabBar Items
+            //Items.Add(new TabBar()
+            //{
+            //    Title = "",
+            //    Items =
+            //    {
+            //        new ShellContent()
+            //        {
+            //            ContentTemplate = new DataTemplate(typeof(ContentPage)),
+            //            Route = "",
+            //        },
+            //    }
+            //});
+        }
+
+        //private void OnMenuItemClicked(object sender, EventArgs e)
+        //{
+        //    
+        //}
+    }
+}
