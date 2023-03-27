5aab5935b17f60c5a09369b4e8ebd1397e48870d
Vijay Anand E G <81947404+egvijayanand@users.noreply.github.com>
2023-03-28 03:10:20 +0530

    StartPath property and Menu style change

--- src/MauiTemplatesCLI/MauiAppX/Data/WeatherForecast.cs
+++ src/MauiTemplatesCLI/MauiAppX/Data/WeatherForecast.cs
@@ -12,6 +12,6 @@ namespace MauiApp._1.Data
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-        public string Summary { get; set; }
+        public string? Summary { get; set; }
     }
 }
