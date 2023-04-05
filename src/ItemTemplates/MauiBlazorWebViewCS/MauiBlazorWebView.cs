5584a1af85ef21c38b96423fe7eeada3d34de52c
Vijay Anand E G <81947404+egvijayanand@users.noreply.github.com>
2023-04-06 02:56:28 +0530

    BlazorWebView item templates

---
+++ src/ItemTemplates/MauiBlazorWebViewCS/MauiBlazorWebView.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Components.WebView.Maui;
+
+namespace $rootnamespace$
+{
+    public class $safeitemname$ : ContentPage
+    {
+        public $safeitemname$()
+        {
+            // Below BlazorWebView initialization can be simplified in just a single line
+            // Add reference to the VijayAnand.MauiBlazor.Markup NuGet package
+            // Add this using statement VijayAnand.MauiBlazor.Markup to bring the extension methods to scope
+            // And then invoke the Configure() method on the BlazorWebView instance, example shown below
+            // new BlazorWebView().Configure("wwwroot/index.html", ("#app", typeof(Main), null));
+            var bwv = new BlazorWebView()
+            {
+                // StartPath property supported on .NET 8
+                //StartPath = "/",
+                HostPage = "wwwroot/index.html"
+            };
+
+            bwv.RootComponents.Add(new RootComponent()
+            {
+                Selector = "#app",
+                ComponentType = typeof(Main),
+                Parameters = null
+            });
+
+            Content = new Grid()
+            {
+                Children =
+                {
+                    bwv
+                }
+            };
+        }
+    }
+}
