4377a2ee8bb069b0a20d696cf5df2831a908091d
Vijay Anand E G <81947404+egvijayanand@users.noreply.github.com>
2023-05-18 02:37:21 +0530

    Explicit package reference for library projects

--- src/MauiTemplatesCLI/MauiClassLib/Class1.cs
+++ src/MauiTemplatesCLI/MauiClassLib/Class1.cs
@@ -2,10 +2,11 @@
 {
     public partial class Class1
     {
+#if UseMaui
         public partial void DoSomething();
 
+#endif
 //-:cnd:noEmit
-        // 
 #if ANDROID
         // Code block for Android
 #elif IOS
@@ -17,12 +18,16 @@
 #elif WINDOWS
         // Code block for Windows
 #else
+//+:cnd:noEmit
         // Code block for Non-supported Platforms
+#if UseMaui
         public partial void DoSomething()
         {
             // Could even be like the below exception
             //throw new PlatformNotSupportedException();
         }
+#endif
+//-:cnd:noEmit
 #endif
 //+:cnd:noEmit
     }
