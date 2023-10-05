c4cbd2c56865e89ae9021c3ab651b839ea9845c7
Vijay Anand E G <81947404+egvijayanand@users.noreply.github.com>
2023-10-05 22:08:50 +0530

    VS Extension updated to its corresponding CLI NuGet package

--- src/Extensions/MauiTemplates/MauiTemplates/source.extension.cs
+++ src/Extensions/MauiTemplates/MauiTemplates/source.extension.cs
@@ -11,7 +11,7 @@ namespace MauiTemplates
         public const string Name = ".NET MAUI Project and Item Templates";
         public const string Description = @".NET MAUI Project, Item Templates, and Code Snippets for Visual Studio 2022 (.NET MAUI 6/7 GA and its Service Releases, .NET MAUI 8 RC).";
         public const string Language = "en-US";
-        public const string Version = "4.1.0.0";
+        public const string Version = "4.2.1.0";
         public const string Author = "Vijay Anand E G";
         public const string Tags = "MAUI, iOS, Android, macOS, WinUI, Windows, Desktop, Mobile, Blazor, .NET MAUI, Mac Catalyst, Templates, VS2022, Visual Studio, WinUI3, GA, Code Snippets, Unified, All-in-One, Hybrid, Tizen, Stable, .NET 7, Blazor Hybrid, BlazorWebView, C#, Handlers, Maps, MAUI Maps, MVVM, XAML, SR, WinUI, Service Release, .NET 6, Dual Screen, Foldable, Surface Duo, General Availability, Markup, Fluent, C# Markup, Media, Audio, Video, Playback, Streaming, AV, Class, .NET 8, Razor, Comet, Reactor, Compiled, Bindings, MVU, F#, Fabulous";
     }
