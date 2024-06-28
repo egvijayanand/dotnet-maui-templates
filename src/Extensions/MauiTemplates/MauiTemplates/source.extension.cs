daf5f9949e6e1496dcf53a4714c99cb5c1585f39
Vijay Anand E G <81947404+egvijayanand@users.noreply.github.com>
2024-06-28 22:55:54 +0530

    Added Apple Privacy Manifest for macOS apps and Maps package in Class Library project #231 #233

--- src/Extensions/MauiTemplates/MauiTemplates/source.extension.cs
+++ src/Extensions/MauiTemplates/MauiTemplates/source.extension.cs
@@ -11,7 +11,7 @@ namespace VijayAnand.MauiTemplates
         public const string Name = ".NET MAUI Project and Item Templates";
         public const string Description = @".NET MAUI Project, Item Templates, and Code Snippets for Visual Studio 2022 (.NET MAUI 6/7/8 GA and its Service Releases and .NET 9 Previews).";
         public const string Language = "en-US";
-        public const string Version = "5.6.2.0";
+        public const string Version = "5.7.0.0";
         public const string Author = "Vijay Anand E G";
         public const string Tags = "MAUI, iOS, Android, macOS, WinUI, Windows, Desktop, Mobile, Blazor, .NET MAUI, Mac Catalyst, Templates, VS2022, Visual Studio, WinUI3, GA, Code Snippets, Unified, All-in-One, Hybrid, Tizen, Stable, .NET 7, Blazor Hybrid, BlazorWebView, C#, Handlers, Maps, MAUI Maps, MVVM, XAML, SR, WinUI, Service Release, .NET 6, Dual Screen, Foldable, Surface Duo, General Availability, Markup, Fluent, C# Markup, Media, Audio, Video, Playback, Streaming, AV, Class, .NET 8, Razor, Comet, Reactor, Compiled, Bindings, MVU, F#, Fabulous, SR1, SR2, Generic, .NET 9, CPM, NuGet";
     }
